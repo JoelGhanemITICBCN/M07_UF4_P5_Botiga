8b3151135b3ddf458b36c8b626c9bf8a66c7c1ea
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-04-13 14:35:28 +0200

    Serializers i canvi de db per que he hagut de fer un migrate

--- botiga_grupD/catalog/serializers.py
+++ botiga_grupD/catalog/serializers.py
@@ -1,14 +1,13 @@
-from django.contrib.auth.models import Group, User
 from rest_framework import serializers
-from botiga_grupD.catalog.models import *
+from .models import *
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+class ProducteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Producte
         fields = ['__all__']
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class CatalegSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cataleg
         fields = ['__all__']
