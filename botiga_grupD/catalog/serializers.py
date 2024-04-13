b8f3cf0462b33097cb9ac48cca0ddbfacbfcd8e1
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-04-13 15:23:55 +0200

    Arreglat el serializers

--- botiga_grupD/catalog/serializers.py
+++ botiga_grupD/catalog/serializers.py
@@ -1,13 +1,14 @@
 from rest_framework import serializers
 from .models import *
 
+#Defineix que es mostra a les trucades de la api
 class ProducteSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Producte
-        fields = ['__all__']
+        fields = '__all__'
 
 
 class CatalegSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Cataleg
-        fields = ['__all__']
+        fields = '__all__'
