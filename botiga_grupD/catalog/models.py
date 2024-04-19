abbcb03ff92efaa890bf9e3259cf89d7a9556303
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-04-19 16:21:34 +0200

    Afegits els models de Targeta i Usuari

--- botiga_grupD/catalog/models.py
+++ botiga_grupD/catalog/models.py
@@ -13,3 +13,15 @@ class Cataleg(models.Model):
     id_producte = models.ForeignKey(Producte, on_delete=models.CASCADE)
     nom = models.CharField(max_length=255)
     categoria = models.CharField(max_length=255)
+
+class Targeta(models.Model):
+    num_targeta = models.CharField(max_length=255)
+    caducitat = models.DateField()
+    codi_seguretat = models.IntegerField()
+
+class Usuari(models.Model):
+   nom = models.CharField(max_length=255)
+   cognom = models.CharField(max_length=255)
+   validat = models.BooleanField()
+   id_targeta = models.ForeignKey(Targeta, on_delete=models.CASCADE)
+   passwd = models.CharField(max_length=255)
