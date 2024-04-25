16e865327f0d8850958fa4f419afa45b7f248e71
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-04-25 18:41:44 +0200

    Afegit el crud de cataleg i poc modificades les views de productes

--- botiga_grupD/catalog/views.py
+++ botiga_grupD/catalog/views.py
@@ -13,6 +13,52 @@ def hello_world(request):
         return Response({"message": "DELETE request received"})
 
 #Defineix com s-han de mostrar les views de la api a django
+
+#Views de cataleg
+@api_view(['GET'])
+def mostrar_cataleg(request, cataleg_id=None):
+    if cataleg_id:
+        try:
+            cataleg = Cataleg.objects.get(id=cataleg_id)
+            serializer = CatalegSerializer(cataleg)
+            return Response(serializer.data, status=200)
+        except Cataleg.DoesNotExist:
+            return Response({'error': 'No s ha trobat el cataleg que busques'}, status=404)
+    else:
+        catalegs = Cataleg.objects.all()
+        serializer = CatalegSerializer(catalegs, many=True)
+        return Response(serializer.data, status=200)
+
+@api_view(['POST'])
+def afegir_cataleg(request, cataleg_id=None):
+    serializer = CatalegSerializer(data=request.data, context={'request': request})
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
+
+@api_view(['PUT'])
+def actualitzar_cataleg(request, cataleg_id=None):
+    cataleg = Cataleg.objects.get(id=cataleg_id)
+    serializer = CatalegSerializer(cataleg, data=request.data, context={'request': request})
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=200)
+    return Response(serializer.errors, status=400)
+
+@api_view(['DELETE'])
+def eliminar_cataleg(request, cataleg_id=None):
+    try:
+        cataleg = Cataleg.objects.get(id=cataleg_id)
+    except Cataleg.DoesNotExist:
+        return Response({'error': 'No s ha trobat el cataleg que busques'}, status=404)
+
+    serializer = CatalegSerializer(cataleg)
+    cataleg.delete()
+
+    return Response(serializer.data, status=200)
+
+#Views de productes
 @api_view(['GET'])
 def mostrar_producte(request, producte_id=None):
     if producte_id:
@@ -26,24 +72,23 @@ def mostrar_producte(request, producte_id=None):
         productes = Producte.objects.all()
         serializer = ProducteSerializer(productes, many=True)
         return Response(serializer.data, status=200)
+
 @api_view(['POST'])
-#Funcio per afegir productes
 def afegir_producte(request, producte_id=None):
-        serializer = ProducteSerializer(data=request.data,context={'request':request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.data, status=400)
+    serializer = ProducteSerializer(data=request.data, context={'request': request})
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=201)
+    return Response(serializer.errors, status=400)
 
 @api_view(['PUT'])
-#Funcio per actualitzar productes
 def actualitzar_producte(request, producte_id=None):
-        producte = Producte.objects.get(id=producte_id)
-        serializer = ProducteSerializer(producte, data=request.data, context={'request': request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=200)
-        return Response(serializer.errors, status=400)
+    producte = Producte.objects.get(id=producte_id)
+    serializer = ProducteSerializer(producte, data=request.data, context={'request': request})
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=200)
+    return Response(serializer.errors, status=400)
 
 @api_view(['DELETE'])
 def eliminar_producte(request, producte_id=None):
@@ -52,11 +97,7 @@ def eliminar_producte(request, producte_id=None):
     except Producte.DoesNotExist:
         return Response({'error': 'No s ha trobat el producte que busques'}, status=404)
 
-    # Serializar el producto antes de eliminarlo
     serializer = ProducteSerializer(producte)
-
-    # Eliminar el producto
     producte.delete()
 
-    # Devolver los datos serializados del producto eliminado
     return Response(serializer.data, status=200)
