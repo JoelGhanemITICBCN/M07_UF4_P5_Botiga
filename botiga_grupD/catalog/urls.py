d358a5e01362c2b6083a7e50a54ec4a9dc34d723
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-04-25 18:35:39 +0200

    Retoque de las urls, quito imports innecesarios y la admin

--- botiga_grupD/catalog/urls.py
+++ botiga_grupD/catalog/urls.py
@@ -14,12 +14,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
-from . import views
-
+from django.urls import  path
+from  catalog import views
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('hello/', views.hello_world, name='hello_world'),
     #Paths per poder fer el crud mitjancant post dels productes i el cataleg
     path('Aproductes/', views.afegir_producte, name='Aproducte'),
