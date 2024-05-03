54b1679824cb9609553a705159144ddfff3bb5a5
JoelGhanemITICBCN <2023_joel.ghanem@iticbcn.cat>
2024-05-03 15:48:56 +0200

    Arreglat el models.py

--- botiga_grupD/pagaments/models.py
+++ botiga_grupD/pagaments/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Usuari(models.Model):
+    nom = models.CharField(max_length=255)
+    password = models.CharField(max_length=255)
+    tarjeta = models.CharField(max_length=255)
