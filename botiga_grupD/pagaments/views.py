f3fa01f1b4e138c27e96ecb9f9d9a63c14c122d0
Joel Ghanem <2023_joel.ghanem@iticbcn.cat>
2024-05-08 19:21:58 +0200

    Comentaris a les views i esborrada una view que sobrava

--- botiga_grupD/pagaments/views.py
+++ botiga_grupD/pagaments/views.py
@@ -6,45 +6,46 @@ from .serializers import *
 from .serializers import FakeCarretoSerializer
 
 # Create your views here.
+#View per modificar el valor de pagat
 @api_view(['PUT'])
 def paga(request, pk):
+    #Cas de que no troba el carreto
     try:
         paga = FakeCarreto.objects.get(id=pk)
     except FakeCarreto.DoesNotExist:
         return Response({'error': 'FakeCarreto not found'}, status=404)
 
-    serializer = MostraFakeSerializer(paga, data=request.data, context={'request': request})
+    #Cas en el que troba el carreto 
+    serializer = FakeCarretoSerializer(paga, data=request.data, context={'request': request})
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=200)
     return Response(serializer.errors, status=400)
 
+#Mostra tots els carretons
 @api_view(['GET'])
-def mostraCarretoFake(request, category_id=None):
+def mostraCarretons(request, category_id=None):
         mostra = FakeCarreto.objects.all()
         serializer = MostraFakeSerializer(mostra, many=True)
         return Response(serializer.data, status=200)
 
-@api_view(['GET'])
-def get_fake_carretos(request):
-    fake_carretos = FakeCarreto.objects.all()
-    serializer = FakeCarretoSerializer(fake_carretos, many=True)
-    return Response(serializer.data, status=200)
-
+#Crea un carreto Fake
 @api_view(['POST'])
 def create_fake_carreto(request):
-    serializer = FakeCarretoSerializer(data=request.data)
+    serializer = MostraFakeSerializer(data=request.data)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=201)
     return Response(serializer.errors, status=400)
 
+#Mostra un usuari
 @api_view(['GET'])
 def get_usuaris(request):
     usuaris = Usuari.objects.all()
     serializer = UsuariSerializer(usuaris, many=True)
     return Response(serializer.data, status=200)
 
+#Crea un Usuari
 @api_view(['POST'])
 def create_usuari(request):
     serializer = UsuariSerializer(data=request.data)
