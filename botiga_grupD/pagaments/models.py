348a757bb9a12a30726848daa583044f492b471a
Joel Ghanem <2023_joel.ghanem@iticbcn.cat>
2024-05-08 19:09:07 +0200

    Comentaris adients als models de pagaments

--- botiga_grupD/pagaments/models.py
+++ botiga_grupD/pagaments/models.py
@@ -1,17 +1,22 @@
 from django.db import models
 
 # Create your models here.
+#Model d usuari per donar li complexitat a pagaments
 class Usuari(models.Model):
     nom = models.CharField(max_length=255)
     password = models.CharField(max_length=255)
     tarjeta = models.CharField(max_length=255)
 
+# Model del carreto temporal per provar que tot funciona, tots els camps son atrezzo menys el pagat
 class FakeCarreto(models.Model):
     producte = models.CharField(max_length=255)
     preu = models.IntegerField()
     quantitat = models.IntegerField()
     usuari = models.ForeignKey(Usuari, on_delete=models.CASCADE)
     pagat = models.BooleanField(default=False)
+
+#Exemples del JSON per fer mes facil la insercio de dades
+
 '''
 {
     "Usuari": {
@@ -24,6 +29,9 @@ class FakeCarreto(models.Model):
         "preu": 99,
         "quantitat": 1,
         "usuari": 1,
+        "pagat": false
+    }
+    {
         "pagat": false
     }
 }
