b2bed2733f671bb830a44b92516ca867426cc0ea
Joel Ghanem <2023_joel.ghanem@iticbcn.cat>
2024-05-08 19:10:28 +0200

    Comentaris addients de les urls

--- botiga_grupD/pagaments/urls.py
+++ botiga_grupD/pagaments/urls.py
@@ -7,12 +7,12 @@ urlpatterns = [
     path('mostra/', views.mostraCarretoFake, name='mostraCarretoFake'),
     # Paga un FakeCarreto
     path('paga/<int:pk>', views.paga, name='paga'),
-    # Get all FakeCarretos
+    # Mostra tots els FakeCarretos
     path('fake_carretos/', views.get_fake_carretos, name='get_fake_carretos'),
-    # Create a new FakeCarreto
+    # Crea un FakeCarreto
     path('fake_carretos/create/', views.create_fake_carreto, name='create_fake_carreto'),
-    # Get all Usuaris
+    # Mostra tots els Usuaris
     path('usuaris/', views.get_usuaris, name='get_usuaris'),
-    # Create a new Usuari
+    # Crea un nou Usuari
     path('usuaris/create/', views.create_usuari, name='create_usuari'),
 ]
