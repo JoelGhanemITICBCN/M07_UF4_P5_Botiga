74645284e9da8a2f48113abd5b5d4d1dfb69dbdf
Joel Ghanem <2023_joel.ghanem@iticbcn.cat>
2024-05-08 19:21:25 +0200

    Comentaris i millora de urls per que siguin molt mes intuitives

--- botiga_grupD/pagaments/urls.py
+++ botiga_grupD/pagaments/urls.py
@@ -4,15 +4,13 @@ from . import views
 
 urlpatterns = [
     # Mostra Tots els FakeCarretons
-    path('mostra/', views.mostraCarretoFake, name='mostraCarretoFake'),
+    path('mostra/', views.mostraCarretons, name='mostraCarretons'),
     # Paga un FakeCarreto
     path('paga/<int:pk>', views.paga, name='paga'),
-    # Mostra tots els FakeCarretos
-    path('fake_carretos/', views.get_fake_carretos, name='get_fake_carretos'),
     # Crea un FakeCarreto
-    path('fake_carretos/create/', views.create_fake_carreto, name='create_fake_carreto'),
+    path('creaCarreto/', views.create_fake_carreto, name='create_fake_carreto'),
     # Mostra tots els Usuaris
     path('usuaris/', views.get_usuaris, name='get_usuaris'),
     # Crea un nou Usuari
-    path('usuaris/create/', views.create_usuari, name='create_usuari'),
+    path('creaUsuaris/', views.create_usuari, name='create_usuari'),
 ]
